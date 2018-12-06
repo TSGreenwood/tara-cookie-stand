6d60639a1c0424bf875e400b3e48196756d31f2e
TSGreenwood <greenwood.ts@gmail.com>
2018-12-05 21:34:31 -0800

    populated lists for each location

--- js/cookie.js
+++ js/cookie.js
@@ -1,14 +1,14 @@
 'Use Strict';
 
 var hoursOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
-console.log(hoursOperation.length);
-//sample array [seaTac, # of hours open, min customers/day, max customers/day, avgCookieSale, ranCustAvg, projectedCookies(based on ranCustAvg)
+
 var firstPike = {
   location: '1st and Pike',
-  minCust: 23,
-  maxCust: 65,
+  minCust: 3,
+  maxCust: 24,
   aveCookSale: 1.2,
   cookiesHourly: [],
+  total: 0,
   ranCustAvg: function() {
     return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
   },
@@ -18,38 +18,182 @@ var firstPike = {
     }
 
   },
+  totalProjected: function(){
+    for (var i = 0; i < this.cookiesHourly.length; i++) {
+      this.total = this.total+ this.cookiesHourly[i];
+    }
+    console.log(this.total);
+  },
   //let's render this as an ul in html
   render: function() {
     var pikeList = document.getElementById('pike');
     for (var i = 0; i < hoursOperation.length; i++) {
       var liEl = document.createElement('li');
       liEl.textContent = hoursOperation[i] + ': bake ' + this.cookiesHourly[i] + ' cookies';
-      console.log(liEl);
       pikeList.appendChild(liEl);
     }
-
-
+    var liTot = document.createElement('li');
+    liTot.textContent = 'Daily total: ' + this.total;
+    pikeList.appendChild(liTot);
   }
 };
 
+var seaTac = {
+  location: 'SeaTac Airport',
+  minCust: 23,
+  maxCust: 65,
+  aveCookSale: 6.3,
+  cookiesHourly: [],
+  total: 0,
+  ranCustAvg: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  },
+  projectedCookie: function() {
+    for (var i=0; i < hoursOperation.length; i++) {
+      this.cookiesHourly.push(Math.floor(this.ranCustAvg() * this.aveCookSale));
+    }
+
+  },
+  totalProjected: function(){
+    for (var i = 0; i < this.cookiesHourly.length; i++) {
+      this.total = this.total+ this.cookiesHourly[i];
+    }
+    console.log(this.total);
+  },
+  //let's render this as an ul in html
+  render: function() {
+    var seaTacList = document.getElementById('seaTac');
+    for (var i = 0; i < hoursOperation.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = hoursOperation[i] + ': bake ' + this.cookiesHourly[i] + ' cookies';
+      seaTacList.appendChild(liEl);
+    }
+    var liTot = document.createElement('li');
+    liTot.textContent = 'Daily total: ' + this.total;
+    seaTacList.appendChild(liTot);
+  }
+};
+
+var seattleCenter = {
+  location: 'Seattle Center',
+  minCust: 11,
+  maxCust: 38,
+  aveCookSale: 3.7,
+  cookiesHourly: [],
+  total: 0,
+  ranCustAvg: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  },
+  projectedCookie: function() {
+    for (var i=0; i < hoursOperation.length; i++) {
+      this.cookiesHourly.push(Math.floor(this.ranCustAvg() * this.aveCookSale));
+    }
+
+  },
+  totalProjected: function(){
+    for (var i = 0; i < this.cookiesHourly.length; i++) {
+      this.total = this.total+ this.cookiesHourly[i];
+    }
+    console.log(this.total);
+  },
+  //let's render this as an ul in html
+  render: function() {
+    var seattleCenterList = document.getElementById('seattleCenter');
+    for (var i = 0; i < hoursOperation.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = hoursOperation[i] + ': bake ' + this.cookiesHourly[i] + ' cookies';
+      seattleCenterList.appendChild(liEl);
+    }
+    var liTot = document.createElement('li');
+    liTot.textContent = 'Daily total: ' + this.total;
+    seattleCenterList.appendChild(liTot);
+  }
+};
+
+var capitalHill = {
+  location: '1st and Pike',
+  minCust: 20,
+  maxCust: 38,
+  aveCookSale: 2.3,
+  cookiesHourly: [],
+  total: 0,
+  ranCustAvg: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  },
+  projectedCookie: function() {
+    for (var i=0; i < hoursOperation.length; i++) {
+      this.cookiesHourly.push(Math.floor(this.ranCustAvg() * this.aveCookSale));
+    }
+
+  },
+  totalProjected: function(){
+    for (var i = 0; i < this.cookiesHourly.length; i++) {
+      this.total = this.total+ this.cookiesHourly[i];
+    }
+    console.log(this.total);
+  },
+  //let's render this as an ul in html
+  render: function() {
+    var capitalHillList = document.getElementById('capitalHill');
+    for (var i = 0; i < hoursOperation.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = hoursOperation[i] + ': bake ' + this.cookiesHourly[i] + ' cookies';
+      capitalHillList.appendChild(liEl);
+    }
+    var liTot = document.createElement('li');
+    liTot.textContent = 'Daily total: ' + this.total;
+    capitalHillList.appendChild(liTot);
+  }
+};
+
+var alki = {
+  location: 'Alki',
+  minCust: 2,
+  maxCust: 16,
+  aveCookSale: 4.6,
+  cookiesHourly: [],
+  total: 0,
+  ranCustAvg: function() {
+    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+  },
+  projectedCookie: function() {
+    for (var i=0; i < hoursOperation.length; i++) {
+      this.cookiesHourly.push(Math.floor(this.ranCustAvg() * this.aveCookSale));
+    }
+
+  },
+  totalProjected: function(){
+    for (var i = 0; i < this.cookiesHourly.length; i++) {
+      this.total = this.total+ this.cookiesHourly[i];
+    }
+    console.log(this.total);
+  },
+  //let's render this as an ul in html
+  render: function() {
+    var alkiList = document.getElementById('alki');
+    for (var i = 0; i < hoursOperation.length; i++) {
+      var liEl = document.createElement('li');
+      liEl.textContent = hoursOperation[i] + ': bake ' + this.cookiesHourly[i] + ' cookies';
+      alkiList.appendChild(liEl);
+    }
+    var liTot = document.createElement('li');
+    liTot.textContent = 'Daily total: ' + this.total;
+    alkiList.appendChild(liTot);
+  }
+};
 firstPike.projectedCookie();
+firstPike.totalProjected();
 firstPike.render();
-console.log(firstPike.cookiesHourly);
-// var minCust =  3;
-// var maxCust = 24;
-// var aveCookSale = 1.2;
+seaTac.projectedCookie();
+seaTac.totalProjected();
+seaTac.render();
+seattleCenter.projectedCookie();
+seattleCenter.totalProjected();
+seattleCenter.render();
+capitalHill.projectedCookie();
+capitalHill.totalProjected();
+capitalHill.render();
+alki.projectedCookie();
+alki.totalProjected();
+alki.render();
 
-// function ranCustAvg() {
-//   Math.floor(  Math.random() * (maxCust - minCust) + minCust) * aveCookSale);
-//   console.log(ranCustAvg);
-//   var ranCA= (ranCustAvg);
-//   console.log(ranCA);
-//   return ranCustAvg;
-// }
-// ranCustAvg();
-
-//based on that customer value, I can multiply it by avgCookieSale to return projectedCookies.
-
-//Next, I need to create an <ul> that iterates projectedCookies over each i within hoursOperation.
-
-//Don't declare any variables within for loops. Get a random, make the li, content = random from above, once per hour
