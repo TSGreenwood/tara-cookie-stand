6e262e13ffcedd804d3c760b43cc2898febba2f4
TSGreenwood <greenwood.ts@gmail.com>
2018-12-11 16:12:12 -0800

    successful 'for loop' across store hours columns

--- js/cookie.js
+++ js/cookie.js
@@ -2,6 +2,8 @@
 
 var hoursOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 var allStores =[];
+var storeTable = document.getElementById('projections');
+console.log(storeTable);
 
 var Store = function (location, minCust, maxCust, aveCookSale) {
   this.location = location;
@@ -10,49 +12,57 @@ var Store = function (location, minCust, maxCust, aveCookSale) {
   this.aveCookSale = aveCookSale;
   this.cookiesHourly = [];
   this.total = 0;
-  this.ranCustAvg = function() {
-    return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
-  };
-  this.projectedCookie = function() {
-    for (var i=0; i < hoursOperation.length; i++) {
-      this.cookiesHourly.push(Math.floor(this.ranCustAvg() * this.aveCookSale));
-    }
-  };
-  this.totalProjected = function() {
-    for (var i = 0; i < this.cookiesHourly.length; i++) {
-      this.total = this.total+ this.cookiesHourly[i];
-    }
-  };
-  this.render = function() {
-    this.ranCustAvg();
-    this.projectedCookie();
-    this.totalProjected();
-    allStores.push(this);
-    var row = document.createElement('tr');
-    var col = document.createElement('td');
-    col.textContent = this.location;
-    row.appendChild(col);
-    // for loop for arr
-    // for (var i = 0; i< allStores.length; i++) {
-    //   console.log(allStores[i]);
-    //   for (var j = 0; j < hoursOperation.length; j++) {
-    //     console.log(hoursOperation[j]);
-    //   }
-    }
+  allStores.push(this);
 
-    // creating totals column
-    col = document.createElement('td');
-    col.textContent = this.total;
-    row.appendChild(col);
+  this.projectedCookie();
+  this.totalProjected();
+  this.renderInnerTableData();
+};
 
-    var blah  = document.getElementById('projections');
-    blah.appendChild(row);
+Store.prototype.ranCustAvg = function() {
+  return Math.floor(Math.random() * (this.maxCust - this.minCust) + this.minCust);
+};
 
-  };
-  this.render();
+Store.prototype.projectedCookie = function() {
+  for (var i = 0; i < hoursOperation.length; i++) {
+    this.cookiesHourly.push(Math.floor(this.ranCustAvg() * this.aveCookSale));
+  }
+};
+
+Store.prototype.totalProjected = function() {
+  for (var i = 0; i < this.cookiesHourly.length; i++) {
+    this.total = this.total+ this.cookiesHourly[i];
+  }
+};
+
+Store.prototype.renderInnerTableData = function() {
+
+  var trEl = document.createElement('tr');
+
+  var tdEl = document.createElement('td');
+  tdEl.textContent = this.location;
+
+  trEl.appendChild(tdEl);
+
+  for (var i = 0; i < this.cookiesHourly.length; i++) {
+    tdEl = document.createElement('td');
+    tdEl.textContent = this.cookiesHourly[i];
+    trEl.appendChild(tdEl);
+  }
+
+  tdEl = document.createElement('td');
+  tdEl.textContent = this.total;
+  trEl.appendChild(tdEl);
+
+  storeTable.appendChild(trEl);
 
 
 };
+
+
+
+
+
 
 new Store('Seattle Center', 11,38, 3.7);
 new Store('a', 1,2,3);
